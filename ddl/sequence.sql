5cd3832553c310138cec671c84523818ebbbb6b3
xia <906094554@qq.com>
2025-04-04 10:39:13 +0800

    refactor database schema for short URL service: update column names and add constraints

--- ddl/sequence.sql
+++ ddl/sequence.sql
@@ -1,7 +1,12 @@
 CREATE TABLE IF NOT EXISTS `sequence`
 (
     `id`        BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `stub`      CHAR(1)         NOT NULL DEFAULT '0' COMMENT '填充位',
+    `stub`      CHAR(1)         NOT NULL DEFAULT '0'
+        COMMENT '占位符',
+#通过唯一性约束确保表中仅存在一行数据
+# 利用MySQL的 REPLACE INTO 或
+# INSERT ... ON DUPLICATE KEY UPDATE
+# 操作触发自增ID生成
     `timestamp` TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
     UNIQUE KEY `uniq_stub` (`stub`)
--- ddl/shortUrlMap.sql
+++ ddl/shortUrlMap.sql
@@ -1,15 +1,17 @@
 CREATE TABLE IF NOT EXISTS `short_url_map`
 (
-    `id`        BIGINT UNSIGNED  NOT NULL AUTO_INCREMENT COMMENT '主键',
-    `create_at` DATETIME         NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `id`        BIGINT UNSIGNED  NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+    `create_at` TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `create_by` VARCHAR(64)      NOT NULL DEFAULT 'system' COMMENT '创建者',
+    `update_at` TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     `is_del`    TINYINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否删除：0正常1删除',
-    `lurl`      VARCHAR(2048)    NOT NULL DEFAULT '' COMMENT '长链接',
+    `long_url`  TEXT             NOT NULL DEFAULT '' COMMENT '长链接',
     `md5`       CHAR(32)         NOT NULL DEFAULT '' COMMENT '长链接MD5',
-    `surl`      VARCHAR(11)      NOT NULL DEFAULT '' COMMENT '短链接',
+    `short_url` VARCHAR(11)      NOT NULL DEFAULT '' COMMENT '短链接',
     PRIMARY KEY (`id`),
     INDEX `idx_is_del` (`is_del`),
+    INDEX `idx_create_at` (`create_at`),
     UNIQUE `uniq_md5` (`md5`),
-    UNIQUE `uniq_surl` (`surl`)
+    UNIQUE `uniq_short_url` (`short_url`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 COMMENT ='长短链映射表';
